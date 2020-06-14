11bbfd199e5fd5417afca9e4de7ccd5d9db07974
kevin791 <17621003609@163.com>
2020-06-14 16:32:04 +0800

    练习

--- djtd/djapp/urls.py
+++ djtd/djapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
 
+#为URL名称添加命名空间,方便项目中多应用的情况
+app_name = 'djapp'
+
 urlpatterns = [
     path('',views.index,name='index'),
-    #<int:question_id>捕获URL传入的整数型
-    path('<int:question_id>/',views.detail,name='detail'),
-    # path('<int:question_id>/results/',views.results,name='results'),
-    # path('<int:question_id>/vote/',views.vote,name='vote'),
+    # #<int:question_id>捕获URL传入的整数型
+    # path('<int:question_id>/',views.detail,name='detail'),
+    #改变投票详情视图的URL
+    path('specifics/<int:question_id>/',views.detail,name='detail'),
+    path('<int:question_id>/results/',views.results,name='results'),
+    path('<int:question_id>/vote/',views.vote,name='vote'),
 ]
--- djtd/djapp/views.py
+++ djtd/djapp/views.py
@@ -1,22 +1,41 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .models import Question,Choice
 from django.template import loader
 from django.shortcuts import render
+from django.urls import reverse
 # Create your views here.
 #第一个视图
 # def index(request):
 #     return HttpResponse("Hehllo,world.")
 #实验访问多个页面-------------
-# def detail(request,question_id):
-#     return HttpResponse("You're looking at question %s." %question_id)
+def detail(request,question_id):
+#这里需要输入一个questionID进行查询
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("没有问题")
+    # return render(request,'djapp/detail.html',{'question' : question})
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'djapp/detail.html',{'question': question})
 
-# def results(request,question_id):
-#     response="you're looking at the results of question %s."
-#     return HttpResponse(response % question_id)
+def results(request,question_id):
+    response="you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
 
-# def vote(request,question_id):
-#     return HttpResponse("you're voting on question %s." % question_id)
+def vote(request,question_id):
+    quetion = get_object_or_404(Question,pk = question_id)
+    try:
+        selected_choice = quetion.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,'djapp/detail.html',{
+            'question':quetion,
+            'error_message':"upi dodm't select a choice",
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('djapp:results',args=(question.id,)))
 #--------------------------
 #真正有用的视图----------------
 #我们在 index() 函数里插入了一些新内容，
@@ -37,14 +56,4 @@ def index(request):
     #输出方式三：render():django提供了一个用作【载入末班，填充上下文，再返回由它生成httpresponse对象】的函数
     context = {'latest_question_list':latest_question_list}
     return render(request,'djapp/index.html',context)
-
-def detail(request,question_id):
-    #这段代码有问题，Quwstion不能调用DoesNotExist异常
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("没有问题")
-    # return render(request,'djapp/detail.html',{'question' : question})
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request,'djapp/detail.html',{'question': question})
 #------------------------------
